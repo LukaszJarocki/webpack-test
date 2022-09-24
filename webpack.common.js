e0089265186f89d0d5f1265c10861c8353209fab
LukaszJarocki <93542313+LukaszJarocki@users.noreply.github.com>
2022-09-24 13:50:02 +0200

    add new page

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/contact.html",
+      inject: true,
+      chunks: ["index"],
+      filename: "contact.html",
+    }),
   ],
 };
